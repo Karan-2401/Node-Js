152e40dd2e930a82db71bb29da96a2aef7689a42
Karan Yadav <karan342772@gmail.com>
2024-11-27 00:43:11 +0530

    day1

---
+++ Day1/index.js
@@ -0,0 +1,8 @@
+// const pokemon = require('./utils');
+
+// console.log(pokemon())
+
+import { line } from "./utils.js";
+line.map((x)=>{
+    console.log(x.roll)
+})
---
+++ Day1/utils.js
@@ -0,0 +1,12 @@
+// function pokemon(){
+//     return Math.floor(Math.random()*6) + 1;
+// }
+
+// module.exports= pokemon;
+
+const line = [
+    {name:"karan"},
+    {roll : 36}
+]
+
+export {line}; 
